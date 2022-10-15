a6c7b4f109c7d7872e9d8e0bc1724ff62ac2658c
GugeLucas <lucas.gugelmin@gmail.com>
2022-10-15 04:58:40 -0400

    Type-boolean

---
+++ type-boolean.js
@@ -0,0 +1,11 @@
+//boolean
+
+const primeiroNumero = 5;
+const segundoNumero = 18;
+
+console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const   texto2 = "A";
+
+console.log(texto1 === texto2);
