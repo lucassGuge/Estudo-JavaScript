34d1605f0762e2e25c947b0e840915fe932f6ff1
GugeLucas <lucas.gugelmin@gmail.com>
2022-10-15 09:57:07 -0400

    Create expressos-fun.js

---
+++ expressos-fun.js
@@ -0,0 +1,22 @@
+//declaração de função
+
+function minhaFuncao (num3, num4) {
+    return num3 + num4;
+}
+console.log(minhaFuncao(5 , 6));
+
+//expressão de função
+
+const soma = function (num1, num2) {    
+  return num1 + num2;
+}
+console.log(soma(1, 2));
+
+
+
+//diferança principal
+
+function apresentar () {
+    return "ola";
+}
+console.log(apresentar());
