1bdf4a1933900379fc6060069a4021f1cf7f6d9e
GugeLucas <lucas.gugelmin@gmail.com>
2022-10-15 05:45:46 -0400

    Create conversao.js
    conversão number e String

---
+++ conversao.js
@@ -0,0 +1,21 @@
+//tipo de dado
+//booleanos
+
+//conversão implicita
+
+const numero = 456;
+const numeroString = "456";
+
+
+console.log(numero == numeroString);
+
+console.log(typeof numero);
+console.log(typeof numeroString);
+
+//number e String
+
+const numero1 = 456;
+const numeroString1 = "456";
+
+
+console.log(numero1 + Number (numeroString1));
