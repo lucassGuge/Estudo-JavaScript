882aa8678e8f5e619d25e38fa1b557fc052a4865
GugeLucas <lucas.gugelmin@gmail.com>
2022-10-15 04:53:02 -0400

    type-caracter
    Tipos

---
+++ type-String.js
@@ -0,0 +1,32 @@
+const texti1 = "Olá, Mundo";
+const texto2 = 'Olá, Mundo';
+const senha = "senhaSuperSegura4561";
+
+const citacao = ' leo disse "Oi"';
+console.log(citacao);
+
+
+// concatenação
+
+
+const frase = ' Meu nome é ';
+const meNome = "Juca";
+console.log(frase + meNome);
+     
+
+//comparação
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+console.log(cidade === input); // false
+
+const city = "belo horizonte";
+const input1 = "Belo Horizonte";
+
+const inputMinusculo = input1.toLowerCase();
+
+console.log(city === inputMinusculo); // true
+
+//Quantos caracteres
+const senha1 = "minhaSenha5588"
+console.log(senha1.length) // 14 caracteres
