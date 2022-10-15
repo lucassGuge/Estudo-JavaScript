47fb4c2042d19f96a12b506190d4b67a314b638a
GugeLucas <lucas.gugelmin@gmail.com>
2022-10-15 04:28:42 -0400

    tipo-numero
    tipo numero

---
+++ type-number.js
@@ -0,0 +1,33 @@
+// tipo number
+
+const meuNumero = 3;
+
+const primeiroNumero = 6;
+const segundoNumero = 2;
+
+const operacaoMatematicaSoma = primeiroNumero + segundoNumero; 
+
+console.log(operacaoMatematicaSoma)
+
+
+
+
+const operacaoMatematicaSub = primeiroNumero - segundoNumero; 
+
+console.log(operacaoMatematicaSub)
+
+
+
+
+const operacaoMatematicaDiv = primeiroNumero / segundoNumero; 
+
+console.log(operacaoMatematicaDiv)
+
+// ponto flutiante
+
+const numeroPontoFlutuante = 3.3;
+const segundoPontoFlutuante = .5;
+
+const operacaoFlutuante = numeroPontoFlutuante * segundoPontoFlutuante;
+
+console.log(operacaoFlutuante);
