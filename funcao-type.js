0d93a5448be6c11207f24004a8bd04be69b3b40e
GugeLucas <lucas.gugelmin@gmail.com>
2022-10-15 09:57:04 -0400

    Create funcao-type.js

---
+++ funcao-type.js
@@ -0,0 +1,27 @@
+//funções
+
+//let x = "";
+//console.log(x);
+//x ="oi";
+
+//declara             //string
+function imprimeTexto(texto){
+    console.log(texto);
+}
+
+// executa a função
+
+imprimeTexto("Oi");
+imprimeTexto("Beleza ");
+
+imprimeTexto(soma());
+
+// tres formas de funções
+
+function soma(){    
+    return  2 + 2;
+    
+}
+
+console.log(soma());
+// 
