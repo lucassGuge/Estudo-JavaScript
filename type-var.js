9a24108305beb91fa357d1d348e0571d83d4a799
GugeLucas <lucas.gugelmin@gmail.com>
2022-10-15 05:45:23 -0400

    Create type-var.js
    tipos de variáveis= var, let e const

---
+++ type-var.js
@@ -0,0 +1,49 @@
+//var
+
+var altura = 5;
+var comprimento = 7;
+
+var area = altura * comprimento;
+
+console.log(area);
+
+//let
+
+let primeiroNumero = 7;
+let segundoNumero = 6; 
+
+let area1 = segundoNumero * primeiroNumero;
+
+console.log(area1);
+
+//if e else
+
+
+let forma ='retangulo';
+let Numero1 = 5;
+let Numero2 = 4; 
+let area2;
+if (forma ==='retangulo') {
+    area2 = segundoNumero * primeiroNumero;
+}
+else {
+    area2 = segundoNumero * primeiroNumero / 2;
+}
+
+console.log(area2);
+
+// const
+
+const forma1 = 'quadrado';
+const altura1 = 2;
+const comprimento1 = 6;
+let area3;
+
+if (forma1 === 'quadrado') {
+    area3 = altura1 * comprimento1;
+}
+else {
+    area3 = (altura1 * comprimento1) / 2;
+}
+    console.log(area3);
+
