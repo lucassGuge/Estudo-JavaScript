a0204fa0fb6709f0efac34173901b846ebace5db
GugeLucas <lucas.gugelmin@gmail.com>
2022-10-15 18:30:53 -0400

    Create lista-2dimensao.js

---
+++ lista-2dimensao.js
@@ -0,0 +1,7 @@
+const alunos = ['João', 'Juliana', 'Ana' ]
+
+const mediaDosAlunos = [ 10, 7, 4]
+
+let listaDeNotasEAlunos = [alunos, mediaDosAlunos]
+
+console.log(`${listaDeNotasEAlunos [0][0]}, sua media é ${listaDeNotasEAlunos [1][0]},)`);
