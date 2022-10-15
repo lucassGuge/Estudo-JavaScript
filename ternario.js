46353fc07bbaca40e504b1e51491f1f8d6636417
GugeLucas <lucas.gugelmin@gmail.com>
2022-10-15 07:29:35 -0400

    Create ternario.js

---
+++ ternario.js
@@ -0,0 +1,15 @@
+ 
+ const idadeMinima = 18;
+ const idadeCliente = 16;
+
+ if (idadeCliente >= idadeMinima) {
+    console.log("Cerveja") 
+ } else {
+    console.log("Suco")
+ }
+
+ // cond
+ const idadeMinima1 = 18;
+ const idadeCliente1 = 22;
+              // condição                     //true      //false //
+ console.log(idadeCliente1 >= idadeMinima1 ? "cerveja" : "Suco"); 
