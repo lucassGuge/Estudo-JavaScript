0df406a8b956d93fdf8ea5fd66adcc71ad47ac1c
GugeLucas <lucas.gugelmin@gmail.com>
2022-10-15 07:29:48 -0400

    Create comparacao.js

---
+++ comparacao.js
@@ -0,0 +1,14 @@
+const numero = 8;
+const texto = "8";
+
+ console.log( numero == texto );
+ console.log( numero === texto );
+
+//-typeof
+
+console.log( typeof texto );
+console.log( typeof numero );
+
+//- coversao explicita
+Number();
+String();
