14d50568c397e5853785c01aa5b083bac7f27773
GugeLucas <lucas.gugelmin@gmail.com>
2022-10-15 18:30:59 -0400

    Create concatenado-sala.js

---
+++ concatenado-sala.js
@@ -0,0 +1,17 @@
+
+const salaDePython = ['João', 'Juliana', 'Ana' ]
+
+const salaDeJava = ['Caio', 'Lara', 'Marjore']
+
+const salaUnica = salaDePython.concat(salaDeJava)
+
+console.log(salaUnica);
+
+//atividade
+
+
+const arrayOriginal = ["Maria", "Carlos", "Eduardo", "Samanta"]
+const arrayConcat = arrayOriginal.concat("André", "Fernanda")
+
+console.log(arrayConcat)
+console.log(arrayOriginal)
