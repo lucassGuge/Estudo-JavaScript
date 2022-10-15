60858bc5478624e150195d2bd09e544396db164e
GugeLucas <lucas.gugelmin@gmail.com>
2022-10-15 07:29:45 -0400

    Create console.js

---
+++ console.js
@@ -0,0 +1,12 @@
+
+// log -> registro
+
+const minhaVar = true;
+
+
+console.log("Oi Meu Chapa");
+console.log(minhaVar);  
+console.log(155);
+
+///--- tratamento erro!!
+console.error('Deu ruim!');
