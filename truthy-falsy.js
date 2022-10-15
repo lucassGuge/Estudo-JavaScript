9ec1105a9f503b30bd98a9fb85d886e1ed30a212
GugeLucas <lucas.gugelmin@gmail.com>
2022-10-15 05:46:01 -0400

    Create truthy-falsy.js

---
+++ truthy-falsy.js
@@ -0,0 +1,22 @@
+//bolean
+const usuarioLogado = true;
+const usuarioDesLogado = false;
+
+// truthy e falsy
+
+console.log(0 == false);
+console.log(1 == false);
+
+//null
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar)
+console.log(varNull)
+
+let numero = 3;
+let texto = 'Alura';
+
+console.log(typeof numero);
+console.log(typeof texto);
