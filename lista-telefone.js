a60f0af0ae116157ac02faa19aec0625a9b94f52
GugeLucas <lucas.gugelmin@gmail.com>
2022-10-17 11:37:04 -0400

    Create lista-telefone.js

---
+++ lista-telefone.js
@@ -0,0 +1,14 @@
+// adicionar mais de 1 telefone
+
+const cliente ={
+    nome : "andre",
+    idade: 36   ,
+    cpf: "856-885-658-35",
+    email: "andre@andre.com",
+    fones: ["8585-8585","6974-3652"]
+
+}
+
+cliente.fones.forEach(fone => console.log(fone));
+
+cliente.fones.forEach(fone => console.log(fone ));
