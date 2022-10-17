05c95c56bcf76991c560ee00f8d946e4c6979dff
GugeLucas <lucas.gugelmin@gmail.com>
2022-10-17 11:37:06 -0400

    Create inserir-campos.js

---
+++ inserir-campos.js
@@ -0,0 +1,20 @@
+// Adicionar campos
+
+const cliente ={
+    nome : "andre",
+    idade: 36   ,
+    cpf: "856-885-658-35",
+    email: "andre@andre.com"
+
+}
+
+
+console.log(cliente);
+
+cliente.fone = "9999-9999"
+
+console.log(cliente);
+
+cliente.fone = "8888-8888"
+
+console.log(cliente);
