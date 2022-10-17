ec1246e50b7078e9a35131f65ed7b108c6c0e665
GugeLucas <lucas.gugelmin@gmail.com>
2022-10-17 11:37:00 -0400

    Create objetos.js

---
+++ objetos.js
@@ -0,0 +1,17 @@
+//objetos
+
+const listaCPFs =["888-888-888-88", "777-777-777-77", "666-666-666-66"]
+
+//array
+const cliente1 = ["nome", "André", "idade", 36]
+
+
+
+//Objeto
+const cliente ={
+    nome : "andre",
+    idade: 36   ,
+    cpf: "856-885-658-35",
+    email: "andre@andre.com"
+
+}
