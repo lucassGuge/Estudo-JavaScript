e118993d979a4822a3fd8c8a9cb445019d9ce3fe
GugeLucas <lucas.gugelmin@gmail.com>
2022-10-18 04:55:55 -0400

    Create lista-unica.js

---
+++ lista-unica.js
@@ -0,0 +1,32 @@
+//
+const cliente = [{
+    nome: 'André',
+    idade: 36,
+    cpf: '2548657962',
+    email:'andre@email.com',
+    fones: ['556588889999','556599887755'],
+    dependentes: [
+        {
+            nome:'Sara silva',
+            parentesco: 'filha',
+            dataNasc: '03/05/2011'
+        },
+        {
+            samia: 'Samia Silva',
+            parentesco: 'filha',
+            dataNasc: '23/09/2008',
+        }
+    ]}, {
+        nome: 'Juliana',
+        idade: 36,
+        cpf: '2548657962',
+        email:'andre@email.com',
+        fones: ['556588889999','556599887755'],
+        dependentes: [
+            {
+                nome:'Sara silva',
+                parentesco: 'filha',
+                dataNasc: '03/05/2011'
+            }
+    ]},
+]
